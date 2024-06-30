cb4f299eae9e342bb48f45f09b63cb601a60416b
shemi <amittpb@gmail.com>
2024-06-30 09:21:12 +0300

    did stuff

--- Course.py
+++ Course.py
@@ -1,28 +1,30 @@
 class Course:
 
     def __init__(self, name, teacher):
-        pass
+        self.name = name
+        self.teacher = teacher
+        self.grademap = []
 
     # adds the student and his grade to the course
     # need to also add the course to the studant object
     def addStudent(self, student, grade):
-        pass
+        self.grademap[student] = grade
 
     def getStudentGrade(self, student):
-        pass
-        
+        return self.grademap.get(student)
+
     #returns the average of the course
     def getAverage(self):
-        pass
+        return sum(self.grademap.values()) / len(self.grademap.values())
 
     #returns the median of the course
     def getMedian(self):
-        pass
+        return sorted(self.grademap.values())[len(self.grademap.values()) // 2]
 
     #returns the prectenge of falis in this class
     def getFailPrecentage(self):
-        pass
+        return filter(lambda x: x < 60, self.grademap.values()) / len(self.grademap.values())
 
     #gets int k and returns the k largest grade in the course.
     def getKgrade(self, k):
-        pass
+        return sorted(self.grademap.values())[k]
